a99c7c50315edcb65b8ded32b19b959dd9932707
Scott Peterson <sm_peterson@yahoo.com>
2024-10-13 19:26:14 +0000

    update

--- installminiconda.sh
+++ installminiconda.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y wget
+sudo apt-get upgrade -y
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
